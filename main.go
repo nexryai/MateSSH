1d591738dc878842b955a069f38c2f72394d52a0
nexryai <nexryai@sda1.net>
2024-04-19 12:11:34 +0900

    Add root check

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nexryai/MateSSH/internal/server"
 	"github.com/nexryai/MateSSH/internal/setup"
 	"github.com/sethvargo/go-diceware/diceware"
+	"os"
 	"strings"
 )
 
@@ -15,6 +16,11 @@ func main() {
 	fmt.Printf("Starting MateSSH server...\n\n")
 
 	log := logger.GetLogger("main")
+
+	if os.Geteuid() == 0 {
+		log.Fatal("MateSSH must not be run as root")
+		os.Exit(1)
+	}
 
 	if !config.IsExist() {
 		// Generate host key
